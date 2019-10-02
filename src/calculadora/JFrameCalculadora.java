d69cf29c3d30b771b78b42488dfce91b9326802a
CRUZLEIJA <CRUZLEIJA@DESKTOP-BIJD7K4>
2019-10-02 12:51:46 -0500

    - gui terminada

--- src/calculadora/JFrameCalculadora.java
+++ src/calculadora/JFrameCalculadora.java
@@ -9,10 +9,12 @@ package calculadora;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
@@ -27,25 +29,33 @@ public class JFrameCalculadora extends JFrame {
 
     private void inicializarComponentes() {
         // crear el componente para la parte central
-        JPanel panelCentral = new JPanel(new GridLayout(4,4));
+        JPanel panelCentral = new JPanel(new GridLayout(5,4));
         // creamos los botones
         Dimension dim = new Dimension(30,30);
         JButtonC btnC = new JButtonC("C", dim, Color.yellow);
         JButtonC btnCE = new JButtonC("CE", dim, Color.yellow);
-        JButtonC btnMM = new JButtonC("+/-", dim, Color.yellow);
-        JButtonC btnMulti = new JButtonC("X", dim, Color.yellow);
-        JButtonC btn7 = new JButtonC("7", dim, Color.yellow);
-        JButtonC btn8 = new JButtonC("8", dim, Color.yellow);
-        JButtonC btn9 = new JButtonC("9", dim, Color.yellow);
-        JButtonC btnDiv = new JButtonC("/", dim, Color.yellow);
-        JButtonC btn4 = new JButtonC("4", dim, Color.yellow);
-        JButtonC btn5 = new JButtonC("5", dim, Color.yellow);
-        JButtonC btn6 = new JButtonC("6", dim, Color.yellow);
-        JButtonC btnMenos = new JButtonC("-", dim, Color.yellow);
-        JButtonC btn1 = new JButtonC("1", dim, Color.yellow);
-        JButtonC btn2 = new JButtonC("2", dim, Color.yellow);
-        JButtonC btn3 = new JButtonC("3", dim, Color.yellow);
-        JButtonC btnPlus = new JButtonC("+", dim, Color.yellow);
+        JButtonC btnMM = new JButtonC("+/-", dim, Color.GREEN);
+        JButtonC btnMulti = new JButtonC("X", dim, Color.GRAY);
+        JButtonC btn7 = new JButtonC("7", dim, Color.GREEN);
+        JButtonC btn8 = new JButtonC("8", dim,Color.GREEN);
+        JButtonC btn9 = new JButtonC("9", dim, Color.GREEN);
+        JButtonC btnDiv = new JButtonC("/", dim, Color.GRAY);
+        JButtonC btn4 = new JButtonC("4", dim, Color.GREEN);
+        JButtonC btn5 = new JButtonC("5", dim, Color.GREEN);
+        JButtonC btn6 = new JButtonC("6", dim, Color.GREEN);
+        JButtonC btnMenos = new JButtonC("-", dim, Color.GRAY);
+        JButtonC btn1 = new JButtonC("1", dim, Color.GREEN);
+        JButtonC btn2 = new JButtonC("2", dim, Color.GREEN);
+        JButtonC btn3 = new JButtonC("3", dim, Color.GREEN);
+        JButtonC btnPlus = new JButtonC("+", dim, Color.GRAY);
+        JButtonC btnCero = new JButtonC("0", dim, Color.GREEN);
+        JButtonC btnPunto = new JButtonC(".", dim, Color.yellow);
+        JButtonC btnIgual = new JButtonC("=", dim, Color.GRAY);
+        JButtonC btnAcerca = new JButtonC("?", dim, Color.yellow);
+       
+       
+        
+        
         panelCentral.add(btnC);
         panelCentral.add(btnCE);
         panelCentral.add(btnMM);
@@ -62,9 +72,24 @@ public class JFrameCalculadora extends JFrame {
         panelCentral.add(btn2);
         panelCentral.add(btn3);
         panelCentral.add(btnPlus);
+        panelCentral.add(btnCero);
+        panelCentral.add(btnPunto);
+        panelCentral.add(btnIgual);
+        panelCentral.add(btnAcerca);
         
         setLayout(new BorderLayout());
         add(panelCentral,BorderLayout.CENTER);
+        
+        JTextField display = new JTextField();
+        Font auxF = display.getFont();
+        Font nueva = new Font(auxF.getName(),auxF.getStyle(),50);
+        display.setFont(nueva);
+        display.setEditable(false);
+        display.setHorizontalAlignment(JTextField.RIGHT);
+        add(display,BorderLayout.NORTH);
+            
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
     
---
+++ src/calculadora/ListenerVerdes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author CRUZLEIJA
+ */
+public class ListenerVerdes implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
